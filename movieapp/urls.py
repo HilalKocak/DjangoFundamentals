8bbbddd97908cbf460a3105fd46ef847b077fd7b
Hilal38 <hhilalkocak@gmail.com>
2023-03-05 15:10:28 +0300

    redirect and reverse used

--- movieapp/urls.py
+++ movieapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from movieapp.views import index
+from movieapp.views import index, getProductsByCategory, getProductsByCategoryId
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('<int:categoryId>', getProductsByCategoryId, name='getProductsByCategoryId'),
+
+    path('<str:category>', getProductsByCategory, name='getProductsByCategory'),
 ]
--- movieapp/views.py
+++ movieapp/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
+from django.shortcuts import redirect, render, HttpResponse
+from django.urls import reverse
 
 # Create your views here.
-
+data = {
+    'telefon':'telefon kategorisindeki urunler',
+    'bilgisayar':'bilgisayar kategorisindeki urunler',
+    'elektronik':'elektronik kategorisindeki urunler',
+}
 def index(request):
     return HttpResponse('Hello')
 
+def getProductsByCategory(request, category):
+    try:
+        category_text=data[category]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound('yanlis kategori')
+
+def getProductsByCategoryId(request, categoryId):
+    category_list = list(data.keys())
+    if categoryId > len(category_list):
+        return HttpResponseNotFound('yanlis kategori secimi')
+    
+    category_name = category_list[categoryId-1]
+    redirected_path = reverse('getProductsByCategory', args=[category_name])
+    return redirect(redirected_path)
